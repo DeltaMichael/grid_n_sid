13c534da145c0163f612d325a0bd4674bf70f99e
Dilyan Marinov <delta_michael@protonmail.com>
2024-04-14 01:00:07 +0300

    Abstract tetrimo in own class

--- src/snid/tetris.py
+++ src/snid/tetris.py
@@ -3,58 +3,94 @@ from random import choice
 from keyboard import add_hotkey
 
 
+class Tetrimo:
+
+    def rotate(self):
+        pass
+
+
+class T(Tetrimo):
+
+    def __init__(self, y, x, variant=None):
+        self.y = y
+        self.x = x
+
+        if variant:
+            self._variant = variant
+        else:
+            self._variant = choice([0, 1, 2, 3])
+        '''
+        ***
+         *
+
+         *
+        **
+         *
+
+         *
+        ***
+
+         *
+         **
+         *
+        '''
+        self._variants = [[(y, x), (y, x + 1), (y, x - 1), (y + 1, x)],
+                          [(y, x), (y + 1, x), (y + 1, x - 1), (y + 2, x)],
+                          [(y, x), (y + 1, x + 1), (y + 1, x - 1), (y + 1, x)],
+                          [(y, x), (y + 1, x), (y + 1, x + 1), (y + 2, x)]]
+
+        self._edge_variants = [[(y, x + 1), (y, x - 1), (y + 1, x)],
+                               [(y + 1, x - 1), (y + 2, x)],
+                               [(y + 1, x + 1), (y + 1, x - 1), (y + 1, x)],
+                               [(y + 1, x + 1), (y + 2, x)]]
+
+        self._cells = self._variants[self._variant]
+        self._edges = self._edge_variants[self._variant]
+
+    def bottom_edge(self):
+        return self._edges
+
+    def move_down(self):
+        self.y = self.y + 1
+        self._cells = [(y + 1, x) for y, x in self._cells]
+        self._edges = [(y + 1, x) for y, x in self._edges]
+
+    def rotate(self):
+        self._variant = (self._variant + 1) % 4
+        self._cells = self._variants[self._variant]
+        self._edges = self._edge_variants[self._variant]
+
+
 class Tetris(GridGame):
 
-    def tetrimo(self):
-        self._cur_tetrimo = choice(self._all_tetrimos)
-        self._grid.set_cells(self._cur_tetrimo)
-
-    def _bottom_edge(self):
-        bottom_row = max(self._cur_tetrimo)
-        edge = [(y, x) for y, x in self._cur_tetrimo if y == bottom_row[0]]
-        return edge
+    def tetrimo(self, y, x):
+        self._template = choice(self._all_tetrimos)
+        self._cur_tetrimo = self._template(y, x)
+        self._grid.set_cells(self._cur_tetrimo._cells)
 
     def _collision(self):
-        for y, x in self._bottom_edge():
+        for y, x in self._cur_tetrimo.bottom_edge():
             if not self._grid._is_cell_valid(y + 1, x):
                 return True
-            if self._grid.get_cell_value((y + 1, x)):
+            if self._grid.get_cell_value((y + 1, x)) == 1:
                 return True
         return False
 
     def _move_tetrimo_down(self):
-        self._grid.clear_cells(self._cur_tetrimo)
-        self._cur_tetrimo = [(y + 1, x) for y, x in self._cur_tetrimo]
-        self._grid.set_cells(self._cur_tetrimo)
-
-    def _move_tetrimo_left(self):
-        next_tetrimo = [(y, x - 1) for y, x in self._cur_tetrimo]
-        for y, x in next_tetrimo:
-            if not self._grid._is_cell_valid(y, x):
-                return
-        self._grid.clear_cells(self._cur_tetrimo)
-        self._cur_tetrimo = next_tetrimo
-        self._grid.set_cells(self._cur_tetrimo)
-
-    def _move_tetrimo_right(self):
-        next_tetrimo = [(y, x + 1) for y, x in self._cur_tetrimo]
-        for y, x in next_tetrimo:
-            if not self._grid._is_cell_valid(y, x):
-                return
-        self._grid.clear_cells(self._cur_tetrimo)
-        self._cur_tetrimo = next_tetrimo
-        self._grid.set_cells(self._cur_tetrimo)
+        self._grid.clear_cells(self._cur_tetrimo._cells)
+        self._cur_tetrimo.move_down()
+        self._grid.set_cells(self._cur_tetrimo._cells)
 
     def _init_state(self):
-        mid = self._grid._w // 2
-        self._all_tetrimos = [
-            [(0, mid), (1, mid), (1, mid - 1), (1, mid + 1)], [(0, mid), (1, mid), (1, mid - 1), (2, mid)], [(0, mid), (1, mid), (1, mid + 1), (2, mid)], [(0, mid), (0, mid + 1), (0, mid - 1), (0, mid - 2)], [(0, mid), (1, mid), (2, mid), (3, mid)]]
-        add_hotkey(4, self._move_tetrimo_left)
-        add_hotkey(37, self._move_tetrimo_right)
-        self.tetrimo()
+        self._mid = self._grid._w // 2
+        self._all_tetrimos = [T]
+        # add_hotkey(4, self._move_tetrimo_left)
+        # add_hotkey(37, self._move_tetrimo_right)
+        # add_hotkey('space', self._rotate_tetrimo)
+        self.tetrimo(0, self._mid)
 
     def _update_state(self):
         if self._collision():
-            self.tetrimo()
+            self.tetrimo(0, self._mid)
         else:
             self._move_tetrimo_down()
