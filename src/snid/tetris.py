c0f9b20f7a51a506c49067da9f4523e89ef52f8b
Dilyan Marinov <delta_michael@protonmail.com>
2024-04-14 01:13:16 +0300

    Rotate with new abstraction

--- src/snid/tetris.py
+++ src/snid/tetris.py
@@ -57,8 +57,10 @@ class T(Tetrimo):
 
     def rotate(self):
         self._variant = (self._variant + 1) % 4
-        self._cells = self._variants[self._variant]
-        self._edges = self._edge_variants[self._variant]
+        self._cells = [(self.y + y, x)
+                       for y, x in self._variants[self._variant]]
+        self._edges = [(self.y + y, x)
+                       for y, x in self._edge_variants[self._variant]]
 
 
 class Tetris(GridGame):
@@ -81,12 +83,17 @@ class Tetris(GridGame):
         self._cur_tetrimo.move_down()
         self._grid.set_cells(self._cur_tetrimo._cells)
 
+    def _rotate_tetrimo(self):
+        self._grid.clear_cells(self._cur_tetrimo._cells)
+        self._cur_tetrimo.rotate()
+        self._grid.set_cells(self._cur_tetrimo._cells)
+
     def _init_state(self):
         self._mid = self._grid._w // 2
         self._all_tetrimos = [T]
         # add_hotkey(4, self._move_tetrimo_left)
         # add_hotkey(37, self._move_tetrimo_right)
-        # add_hotkey('space', self._rotate_tetrimo)
+        add_hotkey('space', self._rotate_tetrimo)
         self.tetrimo(0, self._mid)
 
     def _update_state(self):
